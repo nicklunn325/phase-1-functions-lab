1dd0c16addb59ea4a8d5f45dad9ce9b61ab18ce5
flatlandnick <nicholas.lunn@flatironschool.com>
2022-06-07 10:18:41 -0500

    complete

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
 // Code your solution in this file!
 
+// declare a function named distanceTravelledInFeet
+// define it to accept two parameters, start and destination
+
+function distanceTravelledInFeet(start, destination) {
+  // calculate distance travelled in feet between start and destination
+  // determine number of blocks travelled
+  let blocksTravelled = Math.abs(start - destination);
+  let feetTravelled = blocksTravelled * 264;
+  return feetTravelled;
+}
+
+// declare function accepting 2 parameters
+function calculatesFarePrice(start, destination) {
+  // calculate the distance travelled in feet
+  let feetTravelled = distanceTravelledInFeet(start, destination);
+  console.log(feetTravelled);
+  // if the distance travelled in feet is less than 400 we want to return 0
+  // if the distance is between 400 feet and 2000 multiply feet travelled by .02 - don't include the first 400 feet, those are free
+  // if distance travelled is more than 2000 but less than 2500 return 25
+  // if distance is over 2500 feet no ride
+  if (feetTravelled < 400) {
+    return 0;
+  } else if (feetTravelled < 2000) {
+    return (feetTravelled - 400) * 0.02;
+  } else if (feetTravelled < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+
+calculatesFarePrice;
+
 function distanceFromHqInBlocks(blockNumber) {
   // let blockNumber = 43
   // let blockNumber = 50
